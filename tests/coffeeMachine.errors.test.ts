0ca0989e147e5266d1e7b3c25f0e013fe7057f86
OLIMINATOR <oliverplassiard@proton.me>
2025-10-07 10:27:47 +0200

    testy

--- tests/coffeeMachine.errors.test.ts
+++ tests/coffeeMachine.errors.test.ts
@@ -1,3 +1,27 @@
-describe('CoffeeMachine errors', () => {
+import { Drink } from "../src/drink";
+import { CoffeeMachine } from "../src/coffeeMachine";
 
+describe("CoffeeMachine errors", () => {
+    it("throws too much sugar error", () => {
+        const machine = new CoffeeMachine();
+        const sugarCubes = 8906879567835425;
+        const drink = new Drink("Tea", 2, false, sugarCubes, "small");
+
+        try {
+            machine.serve(drink, 2.1, false, 10);
+        } catch (error) {
+            expect(error).toEqual(new Error("Too much sugar"));
+        }
+    });
+
+    it("throws not enough money error", () => {
+        const machine = new CoffeeMachine();
+        const drink = new Drink("Tea", 452, false, 1, "small");
+
+        try {
+            machine.serve(drink, 45.1, false, 10);
+        } catch (error) {
+            expect(error).toEqual(new Error("Not enough money"));
+        }
+    });
 });
--- tests/notifications.test.ts
+++ tests/notifications.test.ts
@@ -1,3 +1,32 @@
-describe('notifyUser', () => {
+import fetch from "node-fetch";
+import { notifyUser } from "../src/utils/notifications";
 
+jest.mock("node-fetch", () => jest.fn());
+
+jest.useFakeTimers({ now: new Date("2025-01-01") });
+
+describe("notifyUser", () => {
+    it("calls fetch with correct URL, method and payload", async () => {
+        // arrange
+        const message = "Hello World";
+        const fetchSpy = jest.mocked(fetch);
+        fetchSpy.mockResolvedValue({ ok: true } as any);
+
+        // act
+        await notifyUser(message);
+
+        const date = new Date();
+        const formattedDate = date.toISOString();
+
+        // expect
+        expect(fetchSpy).toHaveBeenCalledTimes(1);
+        expect(fetchSpy).toHaveBeenCalledWith(
+            "https://example.com/api/notify",
+            {
+                body: `{"message":"Hello World","timestamp":"${formattedDate}"}`,
+                headers: { "Content-Type": "application/json" },
+                method: "POST",
+            }
+        );
+    });
 });
